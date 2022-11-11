4152b887ea9ea9eb514cf3c668996de6703a2397
中田誠人 <masato.nakata1250softbank.com@gmail.com>
2022-11-11 10:29:38 +0900

    やること一覧ページファイル切り出し

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import './App.css';
-import axios from './Axios';
-import { useState, useEffect } from 'react';
-import requests from './Requests';
+import Router from './routes/Router';
 
 const App = () => {
-  const [posts, setPosts] = useState([])
-  useEffect(() => {
-    axios.get(requests.getPosts)
-      .then(resp => {
-        setPosts(resp.data)
-      })
-      .catch(e => {
-        console.log(e);
-      })
-  }, []);
 
   return (
-    <div>
-      <h1>やること一覧</h1>
-      {posts.map((value, key) => {
-        return (
-          <div key={key}>
-            <p>{value.id}</p>
-            <p>{value.title}</p>
-            <p>{value.content}</p>
-          </div>
-        )
-      })}
-    </div>
+    <>
+      <div>
+        <Router />
+      </div>
+    </>
   );
 }
 
---
+++ src/componets/Posts.js
@@ -0,0 +1,33 @@
+import axios from '../Axios';
+import { useState, useEffect } from 'react';
+import requests from '../Requests';
+
+const Posts = () => {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    axios.get(requests.getPosts)
+      .then(resp => {
+        setPosts(resp.data)
+      })
+      .catch(e => {
+        console.log(e);
+      })
+  }, []);
+
+  return (
+    <div>
+      <h1>やること一覧</h1>
+      {posts.map((value, key) => {
+        return (
+          <div key={key}>
+            <p>{value.id}</p>
+            <p>{value.title}</p>
+            <p>{value.content}</p>
+          </div>
+        )
+      })}
+    </div>
+  );
+}
+
+export default Posts;
